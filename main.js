6061f7bb9acdc331eb825195489aaf12b74a1020
EC2 Default User <ec2-user@ip-172-31-23-112.us-west-1.compute.internal>
2022-06-22 11:10:23 +0000

    ストップウォッチ動画

--- main.js
+++ main.js
@@ -1,16 +1,79 @@
+let display = document.getElementById("display");
 
-//document.getElementById("timer").addEventListener("onclick",stertButton);
-//function stertButton(){
-//$('#timer #startButton').click(function(){
-//$('#timer').html('0:0:0:0');
-/*global timerUp*/
-/*global setIntervalnterval*/
-//timer = setIntervalnterval("Countup()", 100);
-  
-//$(this).attr('disabled' , 'disabled');
 
-var i = 0;
-setIntervalnterval(function(){
-    console.log(i++);
-},1000);
-  
+let startStop = document.getElementById("startStop");
+
+let reset = document.getElementById("reset");
+
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+
+let newHours =0;
+let newMinutes =0;
+let newSeconds =0;
+
+let status="stop";//はじめはlet status;stopから　let status = "stop";
+let interval;
+
+function stopWatch(){
+    
+    seconds++;
+    if(seconds / 60 == 1){
+        minutes++;
+        seconds = 0;
+        if(minutes / 60 == 1){
+            hours++;
+            minutes = 0;
+        }
+    }
+    
+    
+    if(seconds < 10){
+      newSeconds =  "0"+seconds;
+    }else{
+        newSeconds = seconds;
+    }
+    if(minutes < 10){
+        newMinutes="0" + minutes;
+    }else{
+        newMinutes = minutes;
+    }
+ if(hours < 10){
+     newHours = "0" + hours;
+ }else{
+     newHours = hours;
+ }
+    
+    
+    
+    display.innerHTML = newHours+ ":" + newMinutes + ":" + newSeconds;
+    } 
+ 
+ startStop.addEventListener("click",function(){
+     if(status == "stop"){
+     interval = setInterval(stopWatch, 100);//はじめは　letで宣言していたが、上の文でlet interval;しているからlet を消して、stopを起動した。
+   startStop.innerHTML = "STOP";
+    status = "start";
+     }else{
+         clearInterval(interval);
+         startStop.innserHTML = "START";
+         status = "stop";
+     }
+         
+    
+     
+ })
+ 
+ reset.addEventListener("click",function(){
+     clearInterval(interval);
+     startStop.innserHTML = "START";
+         status = "stop";
+    display.innerHTML = "00:00:00";
+    hours = 0;
+    minutes =0;
+    seconds = 0;
+ 
+   })
