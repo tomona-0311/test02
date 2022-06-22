c54029f7545f39f05893ce6e43194a0685930e39
EC2 Default User <ec2-user@ip-172-31-23-112.us-west-1.compute.internal>
2022-06-22 12:46:06 +0000

    ストップウォッチが止まらない

--- main.js
+++ main.js
@@ -2,78 +2,80 @@ let display = document.getElementById("display");
 
 
 let startStop = document.getElementById("startStop");
-
+let stopp = document.getElementById("stopp");
 let reset = document.getElementById("reset");
 
 
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
+let mini = 0;
 
 
-let newHours =0;
-let newMinutes =0;
-let newSeconds =0;
 
 let status="stop";//はじめはlet status;stopから　let status = "stop";
 let interval;
 
 function stopWatch(){
     
-    seconds++;
-    if(seconds / 60 == 1){
-        minutes++;
-        seconds = 0;
-        if(minutes / 60 == 1){
+    mini++;
+    if(mini / 10 == 1){  //miniが１０で割って１になった時、つまり１０になった時を表す。
+        seconds++;
+        mini= 0;
+        if(seconds / 10 == 1){
+            minutes++;
+            seconds = 0;
+            if(minutes / 10 == 1){
             hours++;
             minutes = 0;
+            }
+            
         }
     }
     
     
-    if(seconds < 10){
-      newSeconds =  "0"+seconds;
-    }else{
-        newSeconds = seconds;
-    }
-    if(minutes < 10){
-        newMinutes="0" + minutes;
-    }else{
-        newMinutes = minutes;
-    }
- if(hours < 10){
-     newHours = "0" + hours;
- }else{
-     newHours = hours;
- }
-    
-    
-    
-    display.innerHTML = newHours+ ":" + newMinutes + ":" + newSeconds;
-    } 
+   
+       display.innerHTML = hours+ ":" + minutes + ":" + seconds + ":" + mini;
+} 
  
- startStop.addEventListener("click",function(){
-     if(status == "stop"){
-     interval = setInterval(stopWatch, 100);//はじめは　letで宣言していたが、上の文でlet interval;しているからlet を消して、stopを起動した。
-   startStop.innerHTML = "STOP";
-    status = "start";
-     }else{
-         clearInterval(interval);
-         startStop.innserHTML = "START";
-         status = "stop";
-     }
-         
+ 
+  startStop.addEventListener("click",function(){
+      if(status == "stop"){
+        setInterval(stopWatch, 100);//はじめは　letで宣言していたが、上の文でlet interval;しているからlet を消して、stopを起動した。
+//今この状態でスタートボタンを押すとはじまるよ。
+        status == "start";
+      }
+})
+ stopp.addEventListener("click",function(){
+    if(status == "start"){
+      clearInterval(interval);
+       status = "stop";
+}
+})
+  //   if(status == "stop"){
+   //  interval =
+   //  setInterval(stopWatch, 100);//はじめは　letで宣言していたが、上の文でlet interval;しているからlet を消して、stopを起動した。
+ //  startStop.innerHTML = "STOP";
+   // status = "start";
+   //  }else{
+  //       clearInterval(interval);
+  //       startStop.innserHTML = "START";
+  //       status = "stop";
+     
     
      
- })
+// stopp.addEventListener("click",function(){
+   //  clearInterval(interval);
+   //  status = "stop";
+
  
- reset.addEventListener("click",function(){
-     clearInterval(interval);
-     startStop.innserHTML = "START";
-         status = "stop";
-    display.innerHTML = "00:00:00";
-    hours = 0;
-    minutes =0;
-    seconds = 0;
+reset.addEventListener("click",function(){
+   clearInterval(interval);
+ //    startStop.innserHTML = "START";
+        status = "stop";
+   display.innerHTML = "0:0:0:0";
+  hours = 0;
+   minutes =0;
+   seconds = 0;
  
    })
